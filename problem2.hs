d2481059678a0582392945e39cb3dcb0d3758979
Michal Nestorowicz <michal.nestorowicz@gmail.com>
2020-08-03 12:29:59 +0200

    Shorter implementation of problem2

--- problem2.hs
+++ problem2.hs
@@ -1,12 +1,2 @@
-main = putStrLn (show (fib 4000000))
-
-fib :: Int -> Int
-fib limit = fib' 0 1 limit 0
-
-fib' :: Int -> Int -> Int -> Int -> Int
-fib' a b limit acc
-  | c > limit = acc
-  | otherwise = fib' b c limit newacc
-  where c = a + b
-        is_even = c `mod` 2 == 0
-        newacc = if is_even then acc + c else acc
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+main = putStrLn (show (sum (filter even (takeWhile (<4000000) fibs))))
